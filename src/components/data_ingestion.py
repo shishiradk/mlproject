0db88da3952fcd04775f12027f97ba2b124b44a2
Shishiradk <adhikarishishir11@gmail.com>
2025-05-25 15:36:00 +0545

    hperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,20 +1,22 @@
 import os
 import sys
+from src.exception import CustomException
+from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-
-from src.exception import CustomException
-from src.logger import logging
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', "train.csv")
-    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    test_data_path: str = os.path.join('artifacts', "test.csv")
     raw_data_path: str = os.path.join('artifacts', "data.csv")
 
 class DataIngestion:
@@ -24,14 +26,14 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook/data/stud.csv')
+            df = pd.read_csv(r'notebook\data\stud.csv')  # ✅ FIXED: raw string path
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
-            logging.info("Train test split initiated")
 
+            logging.info("Train test split initiated")
             train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
@@ -51,7 +53,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)  # ✅ fixed extra comma
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
     modeltrainer = ModelTrainer()
-    modeltrainer.initiate_model_trainer(train_arr, test_arr)
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
